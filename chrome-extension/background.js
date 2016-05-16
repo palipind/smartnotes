a1603ac4e5701433af2b297de92671936a3c0fd0
Neel Jain <neeljain198@gmail.com>
2016-05-16 00:01:49 -0400

    SMAR-6: base functionality to set/get user jira domain

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,2 +1,48 @@
-/*
-alert("This is background js")*/
+
+current_user_domain = null; 		//Initially null
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
+	try
+	{
+		if(request.type == "set_domain")
+		{
+			storeJiraDomain(request, sendResponse);
+		}
+		else if(request.type == "get_domain")
+		{
+			getJiraDomain(sendResponse);
+		}
+	}
+	catch(err)
+	{
+		console.log('Error: Message Type or User Domain undefined');
+		sendResponse({type:"Error", emsg: err.message});
+	}
+	return true;		//This is required to tell reciever that we will be sending 
+						//response asynchronously
+});
+
+function storeJiraDomain(msg, sendResponse)
+{
+	if(msg.type == "set_domain")
+	{
+		chrome.storage.sync.set({"user_jira_domain": msg.user_domain});
+		sendResponse({type:"Success"});
+		current_user_domain = msg.user_domain;
+	}
+}
+
+function getJiraDomain(sendResponse)
+{
+	if(current_user_domain)
+	{
+		sendResponse({type:"Success", user_domain:current_user_domain});
+	}
+	else
+	{	
+		chrome.storage.sync.get("user_jira_domain", function(values)
+		{
+			current_user_domain = values.user_jira_domain; //lazy initialization
+			sendResponse({type: "Success", user_domain: current_user_domain});
+		});
+	}
+}
--- chrome-extension/popup/popup.js
+++ chrome-extension/popup/popup.js
@@ -1,1 +1,38 @@
-document.write("Welcome to smart notes")
+var background_port = chrome.runtime.connect({name: "Popupjs"});
+
+window.onload =  function () {
+	
+	//Function to set Text Box with set Jira Domain
+	chrome.runtime.sendMessage({type: "get_domain"}, function(response)
+	{
+		if(response.type == "Error")
+		{
+			console.log("Error: "+ response.emsg);
+			document.getElementById('user_domain').value = "";
+		}
+		console.log("Setting Text with value: " + response.user_domain);
+		document.getElementById('user_domain').value = response.user_domain;
+		
+	});
+
+	document.getElementById('set_domain').onclick = function() {
+		var user_domain = document.getElementById('user_domain').value;
+		chrome.runtime.sendMessage({type: "set_domain", user_domain: user_domain}, function(response)
+		{
+			if(response.type == "Error")
+			{
+				console.log("Error: " + response.emsg);
+				console.log("Resetting the user domain");
+				chrome.runtime.sendMessage({type: "get_domain"}, function(response)
+				{
+					if(response.type = "Error")
+					{
+						document.getElementById('user_domain').value = "";
+					}
+					else
+						document.getElementById('user_domain'). value = response.user_domain;
+				});
+			}
+		});
+	}
+}
