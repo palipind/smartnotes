4699fa49c0e382cd87d8cd63ea8d486b3996d395
Neel Jain <neeljain198@gmail.com>
2016-05-22 00:05:56 -0400

    moving get-set jira domain to new script file

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,11 +1,10 @@
-
-current_user_domain = null; 		//Initially null
+var CURRENT_USER_DOMAIN = null;//Initially null
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	try {
 		if(request.type == "set_domain") {
-			storeJiraDomain(request, sendResponse);
+			jira_domain.storeJiraDomain(request, sendResponse);
 		} else if(request.type == "get_domain") {
-			getJiraDomain(sendResponse);
+			jira_domain.getJiraDomain(sendResponse);
 		}
 	} catch(err) {
 		console.log('Error: Message Type or User Domain undefined');
@@ -14,20 +13,3 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	return true;		//This is required to tell reciever that we will be sending 
 						//response asynchronously
 });
-
-function storeJiraDomain(msg, sendResponse) {
-		chrome.storage.sync.set({"user_jira_domain": msg.user_domain});
-		sendResponse({type:"Success"});
-		current_user_domain = msg.user_domain;
-}
-
-function getJiraDomain(sendResponse) {
-	if(current_user_domain) {
-		sendResponse({type:"Success", user_domain:current_user_domain});
-	} else {	
-		chrome.storage.sync.get("user_jira_domain", function (values) {
-			current_user_domain = values.user_jira_domain; //lazy initialization
-			sendResponse({type: "Success", user_domain: current_user_domain});
-		});
-	}
-}
---
+++ chrome-extension/engine/utility/jira_domain.js
@@ -0,0 +1,21 @@
+var jira_domain = ( function () {
+	return {
+		storeJiraDomain: function (msg, sendResponse) {
+			if(msg.type == "set_domain") {
+				chrome.storage.sync.set({"user_jira_domain": msg.user_domain});
+				sendResponse({type:"Success"});
+				CURRENT_USER_DOMAIN = msg.user_domain;
+			}
+		},
+		getJiraDomain: function (sendResponse) {
+			if(CURRENT_USER_DOMAIN) {
+				sendResponse({type:"Success", user_domain: CURRENT_USER_DOMAIN});
+			} else {	
+				chrome.storage.sync.get("user_jira_domain", function (values) {
+					CURRENT_USER_DOMAIN = values.user_jira_domain; //lazy initialization
+					sendResponse({type: "Success", user_domain: CURRENT_USER_DOMAIN});
+				});
+			}
+		}
+	};
+})();
